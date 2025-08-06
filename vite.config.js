94109343a1872697de8142067827c68b90b03b03
scs199204 <scs199204@gmail.com>
2025-08-06 10:26:54 +0900

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
-// ★ 追加：プラグインをインポート
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  // ★ 修正：プラグインの配列に cssInjectedByJsPlugin() を追加
   plugins: [vue(), cssInjectedByJsPlugin()],
   build: {
     outDir: 'dist',
-    // cssCodeSplitはデフォルト設定のままでOK
     rollupOptions: {
       input: path.resolve(__dirname, 'src/js/config.js'),
       output: {
         entryFileNames: 'js/config.js',
         format: 'umd',
-        name: 'KintoneSumifPlugin',
-        // この設定はシンプルでOK
-        assetFileNames: 'assets/[name]-[hash].[ext]',
+        name: 'KintoneConditionalSumPlugin',
+        assetFileNames: 'assets/[name].[ext]',
       },
     },
     sourcemap: false,
